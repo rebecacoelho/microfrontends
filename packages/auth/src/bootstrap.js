af85761f25c0dcff67d07ca69d80fe3b1f16ea50
Rebeca Coelho <rebecasantoscoelho@gmail.com>
2025-06-09 17:03:25 -0300

    feat: added authentication logic

--- packages/auth/src/bootstrap.js
+++ packages/auth/src/bootstrap.js
@@ -4,14 +4,14 @@ import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el, { onNavigate, defaultHistory }) => {
+const mount = (el, { onNavigate, defaultHistory, onSignIn }) => {
   const history = defaultHistory || createMemoryHistory();
 
   if (onNavigate) {
     history.listen(onNavigate);
   }
 
-  ReactDOM.render(<App history={history} />, el);
+  ReactDOM.render(<App history={history} onSignIn={onSignIn} />, el);
 
   return {
     onParentNavigate({ pathname: nextPathname }) {
